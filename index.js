78dff9cf275898a6f33871df9d23f961b4210c58
s-takada <s-takada@tribalmedia.co.jp>
2016-02-08 18:42:08 +0900

    [EDIT] add request listener & file handler.
    request css, javascript & html files, listener & handler is functional.
    set Content-Type & status code and attach files.

--- index.js
+++ index.js
@@ -1,19 +1,66 @@
-const http = require('http');
+const http     = require('http');
 const socketio = require('socket.io');
-const fs = require('fs');
-const twitter = require('twitter');
-const client = new twitter({
-    consumer_key: '',
-    consumer_secret: '',
-    access_token_key: '',
-    access_token_secret: ''
+const fs       = require('fs');
+const twitter  = require('twitter');
+const path     = require('path');
+const client   = new twitter({
+  consumer_key: '',
+  consumer_secret: '',
+  access_token_key: '',
+  access_token_secret: ''
 });
+
 var stream = null;
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type':'text/html'});
-  res.end(fs.readFileSync('./index.html', 'utf-8'));
-}).listen(3000);
+var server = http.createServer(requestListener);
+server.listen((process.env.PORT || 3000), function() {
+  console.log('Server Starting !!! '(process.env.PORT || 3000));
+});
+
+function requestListener(req, res) {
+
+    var requestURL = req.url;
+    var extensionName = path.extname(requestURL);
+
+    switch(extensionName)
+    {
+        case ".html":
+            readFileHandler(requestURL, "text/html", res);
+            break;
+        case ".css":
+            readFileHandler(requestURL, "text/css", res);
+            break;
+        case ".js":
+            readFileHandler(requestURL, "text/javascript", res);
+            break;
+        default:
+            readFileHandler("/index.html", "text/html", res);
+            break;
+    }
+}
+
+function readFileHandler(fileName, contentType, response) {
+
+    var filePath = __dirname + fileName;
+
+    fs.exists(filePath, function(exits) {
+        if(exits) {
+          fs.readFile(filePath, {encoding: 'utf8'}, function (error, data) {
+            if (error) {
+              response.statusCode = 500;
+              response.end("Internal Server Error");
+            } else {
+              response.statusCode = 200;
+              response.setHeader("Content-Type", contentType);
+              response.end(data);
+            }
+          });
+        } else {
+            response.statusCode = 400;
+            response.end("400 Error");
+        }
+    });
+}
 
 var io = socketio.listen(server);
 
